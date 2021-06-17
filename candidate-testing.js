16c8cde5e32f2cbcfe6c1b15ee4920b54545738d
Steve Thatsanithone <80913966+stevethats@users.noreply.github.com>
2021-06-17 22:12:34 +0000

    tried to please the auto grader.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,28 +3,26 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in space?";
+let correctAnswer = "Sally Ride";;
+let candidateAnswer = "";
 let questions = [
-  "Who was the first American woman in space? ",
-  "True or false: 5 kilometer == 5000 meters? ",
-  "(5 + 3)/2 * 10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "Who was the first American woman in space? ", 
+  "True or false: 5 kilometer == 5000 meters? ", 
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
   "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
-  "Sally Ride",
-  "true",
-  "40",
-  "Trajectory",
+  "Sally Ride", 
+  "true", 
+  "40", 
+  "Trajectory", 
   "3"
 ];
 let candidateAnswers = [];
-
-correctAnswer = "Sally Ride";
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -36,14 +34,11 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
   //candidateAnswer = input.question("Who was the first American woman in space? ");
-
   for (let i = 0; i < questions.length; i++) {
     let answer = input.question(questions[i]);
     candidateAnswers.push(answer);
   }
-
 }
 
 function makeCaseInsensitive(candidateAnswers) {
@@ -65,24 +60,9 @@ function gradeQuiz(candidateAnswers) {
   for (let i = 0; i < candidateAnswers.length; i++) {
     console.log(`${i + 1}) ${questions[i]}\nCorrect Answer: ${correctAnswers[i]}\nYour answer: ${candidateAnswers[i]}\n `);
   }
-  // console.log(`1) ${questions[0]}\nCorrect Answer: ${correctAnswers[0]}\nYour answer: ${candidateAnswers[0]}\n `);
-  // console.log(`Question 2\nCorrect Answer: ${correctAnswers[1]}\nYour answer: ${candidateAnswers[1]}\n `);
-  // console.log(`Question 3\nCorrect Answer: ${correctAnswers[2]}\nYour answer: ${candidateAnswers[2]}\n `);
-  // console.log(`Question 4\nCorrect Answer: ${correctAnswers[3]}\nYour answer: ${candidateAnswers[3]}\n `);
-  // console.log(`Question 5\nCorrect Answer: ${correctAnswers[4]}\nYour answer: ${candidateAnswers[4]}\n `);
-
-  /*
-  if (correctAnswer === candidateAnswer) {
-    console.log("That's correct!");
-  } else {
-    console.log("That's not right.");
-  }
-  */
 
   correctAnswers = makeCaseInsensitive(correctAnswers);
-  //console.log(correctAnswers);
   candidateAnswers = makeCaseInsensitive(candidateAnswers);
-  //console.log(candidateAnswers);
 
   let grade = function(candidateAnswers, correctAnswers) {
     let tally = 0;
@@ -109,8 +89,6 @@ function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
   askQuestion();
-  // console.log(makeCaseInsensitive(candidateAnswers));
-  // console.log(makeCaseInsensitive(correctAnswers));
   gradeQuiz(this.candidateAnswers);
 }
 
